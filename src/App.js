c659a82a7a346b2f15f57b61d680d71c39fdc939
Johan Vermeulen <johan.vermeulen@bettyblocks.com>
2019-01-16 15:19:00 +0100

    change app

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Bye there! LOL!
+            Feature change
           </p>
           <a
             className="App-link"
